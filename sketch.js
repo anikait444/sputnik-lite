3b90e5c77161d8beeb5bde9e160f4a143dd53b0d
anikait444 <76683492+anikait444@users.noreply.github.com>
2021-05-24 05:28:13 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var database;
+var back_img;
+var gameState =0;
+var playerCount = 0;
+var allPlayers;
+
+var player, form,game;
+var player1,player2;
+var players;
+var virus,virusgroup,mask,maskgroup;
+var virus1_img, virus2_img, virus3_img, virus4_img;
+var player_img;
+
+
+function preload(){
+  back_img = loadImage("room.jpg");
+  player1_img = loadImage("warrior2.png");
+  player2_img = loadImage("warrior3.png");
+  virus1_img = loadImage("enemy1.png");
+  virus2_img = loadImage("enemy2.png");
+  virus3_img = loadImage("enemy3.png");
+  virus4_img = loadImage("enemy4.png");
+  mask_img = loadImage("mask.png");
+  vaccine1_img = loadImage("vaccine.png");
+  vaccine2_img =loadImage("vaccine2.png");
+
+  virusgroup = new Group();
+  maskgroup = new Group();
+
+  
+}
+function setup() {
+  createCanvas(1000, 600);
+   database = firebase.database();
+  game = new Game();
+   game.getState()
+  game.start();
+  
+}
+
+function draw() {
+  background(back_img);
+  
+   if (playerCount === 2) {
+     game.update(1);
+   }
+   if (gameState === 1) {
+     clear(); 
+     game.play();
+   }
+   if (gameState === 2) {
+    
+     game.end();
+   }
+}
